51141039b5aa72f39321fc8321635b15cffbeed7
Daniel Gans <daniel.gans@gft.com>
2020-10-04 13:52:53 +0200

    header style added

--- oneMillioBotProject/src/shared/message-center/message-center.component.ts
+++ oneMillioBotProject/src/shared/message-center/message-center.component.ts
@@ -1,14 +1,42 @@
-import { Component } from '@angular/core';
+import {
+  AfterViewChecked,
+  Component,
+  ElementRef,
+  OnInit,
+  ViewChild,
+} from '@angular/core';
 import { MESSAGE_FROM } from 'src/app/enums/messageType.enum';
+
+import { faBars, faMinus } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'omb-message-center',
   templateUrl: './message-center.component.html',
   styleUrls: ['./message-center.component.scss'],
 })
-export class MessageCenterComponent {
+export class MessageCenterComponent implements OnInit, AfterViewChecked {
+  @ViewChild('scrollMe') private bodyScroll: ElementRef;
+  public footer_text = 'by 1millionbot';
+  public botName = 'Bill';
+  public faBars = faBars;
+  public faMinus = faMinus;
+
   constructor() {}
 
-  public setStyleByUserFrom = (userFrom: number) =>
+  public setStyleByUserFrom = (userFrom: number): string =>
     userFrom === MESSAGE_FROM.BOT ? 'bot__style' : 'user__style';
+
+  public scrollToBottom = (): void => {
+    try {
+      this.bodyScroll.nativeElement.scrollTop = this.bodyScroll.nativeElement.scrollHeight;
+    } catch (err) {}
+  };
+
+  ngOnInit(): void {
+    this.scrollToBottom();
+  }
+
+  ngAfterViewChecked(): void {
+    this.scrollToBottom();
+  }
 }
--- oneMillioBotProject/src/shared/shared.module.ts
+++ oneMillioBotProject/src/shared/shared.module.ts
@@ -1,10 +1,13 @@
 import { NgModule } from '@angular/core';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { MessageCenterComponent } from './message-center/message-center.component';
 import { MessageComponent } from './message/message.component';
 
 const COMPONENTS = [MessageComponent, MessageCenterComponent];
+const MODULES = [FontAwesomeModule];
 
 @NgModule({
+  imports: [MODULES],
   declarations: [...COMPONENTS],
   exports: [...COMPONENTS],
 })
